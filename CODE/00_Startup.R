e4edf93871f8253e8b69a6ba5cb7928a9a67435c
Andy Youngdahl <ayoungdahl@gmail.com>
2018-09-02 11:59:56 -0500

    Moved all R package installs to 00 script

--- CODE/00_Startup.R
+++ CODE/00_Startup.R
@@ -13,3 +13,10 @@ devtools::install_github(repo = 'yihui/printr')
 ## Update to RSocrata 1.7.2-2 (or later) 
 ## which is only on github as of March 8, 2016
 devtools::install_github(repo = 'chicago/RSocrata')
+
+## deps for parts 20's and 30's
+install.packages('data.table')
+install.packages('glmnet')
+install.packages('ggplot2')
+install.packages('randomForest')
+install.packages('ROCR')
