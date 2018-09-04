1af93331cd9c0119e9de26e54b9f077ebde3d86e
andy <ayoungdahl@gmail.com>
2018-09-04 12:54:43 -0500

    Modify to remove storage of fie.zip from git repo.  GitHub will not store files of this size

--- scripts/repeat/evaluate.sh
+++ scripts/repeat/evaluate.sh
@@ -7,6 +7,7 @@ PATH=~/.local/bin:$PATH
 # [wf] repeat model evaluation from sciunit repo
 
 if [ ! -d $HOME/sciunit/fie ]; then
+    wget -O $HOME/sciunit/fie.zip https://www.dropbox.com/s/l59dibxkdqj816g/fie.zip?dl=0
     unzip -d $HOME/sciunit fie.zip
 fi
 
